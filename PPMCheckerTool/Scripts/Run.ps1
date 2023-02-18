7a2e489ac89860392d266c3f1bc4568959734f2b
Sidharth Venkatesh <sivenkatesh@microsoft.com>
2023-02-18 02:11:11 -0800

    Initial v1.0 commit

---
+++ PPMCheckerTool/Scripts/Run.ps1
@@ -0,0 +1,177 @@
+﻿[Cmdletbinding()]
+Param(
+    [Parameter(Mandatory = $true)]
+    [string] $ExeLocation,
+
+    [Parameter(Mandatory = $true)]
+    [string] $OutputFilePath,
+
+    [Parameter(Mandatory = $true)]
+    [string] $TargetCPU
+)
+
+# 'ExeLocation' is the folder in which the release for both the PPMCheckerTool project and SetSliderPowerMode project resides. Can be a relative path. e.g. D:\PPMCheckerRelease
+# To publish both projects to the same folder, right click the project in Visual studio -> Publish -> to Folder. Default publish settings works.
+# Otherwise, use the release from the Github page and unzip it. It is the same output as the 'Publish'
+# 'OutputFilePath' is the full location and name of the file. e.g. D:\Tools\output.txt. Can be a relative path.
+
+process
+{
+    Write-Output "Running script...`n"
+
+    Write-Output "Checking if power scheme is Balanced...`n"
+
+    
+    cmd /c "powercfg /getactivescheme" | Tee-Object -Variable CurrentScheme
+
+
+    if( $CurrentScheme -like "*381b4222-f694-41f0-9685-ff5bb260df2e*"){
+        Write-Output "Scheme is Balanced. Continuing script...`n"
+    
+
+        try
+        {
+            cd $ExeLocation -ErrorAction Stop
+        }
+        catch [Exception]{
+            Write-Error $_
+            exit;
+        }
+
+        Write-Output "Current Power mode"
+        & ".\SetSliderPowerMode.exe"
+
+        #####################
+        # BALANCED/RECOMMENDED
+        #####################
+        Write-Output "`nChanging Power mode to  Balanced (Non-Surface) / Recommended(Surface)"
+        & ".\SetSliderPowerMode.exe" Default
+
+        Write-Output "Taking ETW trace..."
+        try
+        {
+            wpr -start $PSScriptRoot\power.wprp
+            Start-Sleep -Seconds 0.5
+            wpr -stop "$PSScriptRoot\power_default.etl"
+        }catch [Exception]{
+            Write-Error $_
+            exit
+        }
+
+        Write-Output "Running PPM Checker for Default Power scheme..."
+    
+        try
+        {
+            & ".\PPMCheckerTool.exe" -i $PSScriptRoot\power_default.etl -o $OutputFilePath -t $TargetCPU
+        }catch [Exception]{
+            Write-Error $_
+            exit;
+        }
+
+        Write-Output "Balanced(Recommended) power scheme done..."
+
+        #####################
+        # BETTER BATTERY
+        #####################
+        Write-Output "`nChanging Power mode to Better Battery"
+        & ".\SetSliderPowerMode.exe" BetterBattery
+
+        Write-Output "Taking ETW trace..."
+        try
+        {
+            wpr -start $PSScriptRoot\power.wprp
+            Start-Sleep -Seconds 0.5
+            wpr -stop "$PSScriptRoot\power_betterbattery.etl"
+        }catch [Exception]{
+            Write-Error $_
+            exit;
+        }
+
+        Write-Output "Running PPM Checker for Better Battery Power scheme..."
+
+        try
+        {
+            & ".\PPMCheckerTool.exe" -i $PSScriptRoot\power_betterbattery.etl -o $OutputFilePath -t $TargetCPU
+        }catch [Exception]{
+            Write-Error $_
+            exit;
+        }
+        Write-Output "Better Battery power scheme done..."
+
+        #####################
+        # BETTER PERFORMANCE
+        #####################
+        Write-Output "`nChanging Power mode to Better Performance"
+        & ".\SetSliderPowerMode.exe" BetterPerformance
+
+        Write-Output "Taking ETW trace..."
+        try
+        {
+            wpr -start $PSScriptRoot\power.wprp
+            Start-Sleep -Seconds 0.5
+            wpr -stop "$PSScriptRoot\power_betterperf.etl"
+        }catch [Exception]{
+            Write-Error $_
+            exit;
+        }
+
+        Write-Output "Running PPM Checker for Better Performance Power scheme..."
+
+        try
+        {
+            & ".\PPMCheckerTool.exe" -i $PSScriptRoot\power_betterperf.etl -o $OutputFilePath -t $TargetCPU
+        }catch [Exception]{
+            Write-Error $_
+            exit;
+        }
+
+        Write-Output "Better Performance power scheme done..."
+
+        #####################
+        # BEST PERFORMANCE
+        #####################
+        Write-Output "`nChanging Power mode to Best Performance"
+        & ".\SetSliderPowerMode.exe" BestPerformance
+
+        Write-Output "Taking ETW trace..."
+        try
+        {
+            wpr -start $PSScriptRoot\power.wprp
+            Start-Sleep -Seconds 0.5
+            wpr -stop "$PSScriptRoot\power_bestperf.etl"
+        }catch [Exception]{
+            Write-Error $_
+            exit;
+        }
+
+        Write-Output "Running PPM Checker for Best Performance Power scheme..."
+
+        try
+        {
+            & ".\PPMCheckerTool.exe" -i $PSScriptRoot\power_bestperf.etl -o $OutputFilePath -t $TargetCPU
+        }catch [Exception]{
+            Write-Error $_
+            exit;
+        }
+
+        Write-Output "Best Performance power scheme done..."
+
+        #####################
+
+        Write-Output "`nRestoring power mode to Balanced"
+        Write-Output "Changing Power mode to Balanced (Non-Surface) / Recommended(Surface)"
+        & ".\SetSliderPowerMode.exe" Default
+
+        try
+        {
+            cd $PSScriptRoot
+        }
+        catch [Exception]{
+            Write-Error $_
+            exit;
+        }
+    }
+    else{
+        Write-Output "ERROR: Scheme is not balanced! Please change the scheme and re-run script`n"
+    }
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+# PPM Checker Tool
+
+Processor Power Management (PPM) settings can significantly impact power and performance of Windows systems. These settings configure getting the right threads on the right cores at the right time and frequency that balance performance and power requirements. 
+Configuring the PPM settings is a complex process and requires area expertise and understanding of each setting’s impact on the system’s behavior. There are many settings with many dimensions (Power Scheme, Power Source, Processor Class, Quality of Service etc.) which create numerous numbers of configurations and add to the complexity of the PPM configuration task.
+
+To help this process, we built a tool that the silicon vendors/OEMs can deploy to validate the PPM settings on their new products.
+
+> The Architecture
+> ![image](https://user-images.githubusercontent.com/121056171/210118102-93b0a087-0562-4b1d-baa3-0ef5aae138ce.png)
+
+## Before you run
+Ensure that you create/copy a PPM Setting Rules XML file. A basic Intel based rules XML is included
+
+## How to use the Automatic Script
+The script is located in the Github release, as well as in the Scripts folder in the PPMCheckerTool.
+### To Run the script with the release
+Download the release from Github. Unzip it. Run the script Run.ps1 from the Scripts folder.
+
+‘ExeLocation’ refers to the location that both the project Exe’s reside – PPMCheckerTool.exe and SetSliderPowerMode.exe. In this case, it is 1 directory up. Enter ‘..’ in the prompt. Can be relative path.
+
+‘OutputFilePath’ refers to the full name and path of the output file. Eg. D:\Outputs\Output.txt will work. Can be relative path.
+
+### To Run the script with the visual studio development environment
+
+Open the solution in Visual Studio, ensure you have no build errors.
+Click on the down arrow next to the 'Run button' (Green arrow). Add the command line parameters -i <trace.etl>  -o <output.csv/txt>  -t <target CPU>
+Run the solution
+
+
+## Contributing
+
+This project welcomes contributions and suggestions.  Most contributions require you to agree to a
+Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
+the rights to use your contribution. For details, visit https://cla.opensource.microsoft.com.
+
+When you submit a pull request, a CLA bot will automatically determine whether you need to provide
+a CLA and decorate the PR appropriately (e.g., status check, comment). Simply follow the instructions
+provided by the bot. You will only need to do this once across all repos using our CLA.
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
+For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
+contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+
+## Trademarks
+
+This project may contain trademarks or logos for projects, products, or services. Authorized use of Microsoft 
+trademarks or logos is subject to and must follow 
+[Microsoft's Trademark & Brand Guidelines](https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/usage/general).
+Use of Microsoft trademarks or logos in modified versions of this project must not cause confusion or imply Microsoft sponsorship.
+Any use of third-party trademarks or logos are subject to those third-party's policies.
